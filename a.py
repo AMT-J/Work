c4069d6a3d46059856a6ec34f0a8ed1e8c8deac7
AMT <78520070+AMT-J@users.noreply.github.com>
2022-06-03 03:30:08 +0000

    test

--- a.py
+++ a.py
@@ -1,1 +1,15 @@
-
+import random
+N=100
+a=[random.randint(1,N) for i in range(N)]
+print(a)
+for i in range(N):
+    for j in range(N-1,i,-1):
+        if a[j-1]>a[j]:
+            a[j-1],a[j]=a[j],a[j-1]
+print(a)
+random.shuffle(a)
+for i in range(N):
+    for j in range(0,N-i-1):
+        if a[j+1]<a[j]:
+            a[j+1],a[j]=a[j],a[j+1]
+print(a)
